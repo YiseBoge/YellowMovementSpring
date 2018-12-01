9e75ededf34dd2843425205bfdab5fcc9b960cd5
yisehakbogale <yisehakb@gmail.com>
2018-12-02 00:24:42 +0300

    Initial Commit

---
+++ src/main/java/com/yellowmovement/site/YellowMovementSiteApplication.java
@@ -0,0 +1,12 @@
+package com.yellowmovement.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowMovementSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowMovementSiteApplication.class, args);
+	}
+}
