380c83d65efa94abc59309ddbdefa50016771c19
yisehakbogale <yisehakb@gmail.com>
2018-12-14 23:59:02 +0300

    jQuery validator added with no full application.

---
+++ src/main/resources/static/js/validation.js
@@ -0,0 +1,26 @@
+$(function () {
+    alert("adfef");
+    $('#loginForm').validate({
+
+        rules:{
+            loginEmail: {
+                required: true,
+                email: true
+            },
+
+            loginPassword:{
+                required: true
+            }
+        },
+        messages:{
+            loginEmail:{
+                required: 'Email is Required to Sign in.',
+                email: 'Please enter a <em>valid</em> email.'
+            },
+            loginPassword:{
+                required: 'Password is also Required to Sign in.'
+            }
+        }
+
+    })
+})
