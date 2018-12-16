2f6a483549c8db87d0eec413de7d38feca40b902
mamushe <tewodroslemma64@gmail.com>
2018-12-16 06:47:29 +0300

    error in loading css due to an attempt to add database resolved

--- src/main/java/com/yellowmovement/site/web/ProfileController.java
+++ src/main/java/com/yellowmovement/site/web/ProfileController.java
@@ -2,13 +2,14 @@ package com.yellowmovement.site.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/profile")
 public class ProfileController {
 	
 	@GetMapping
-	public String profile() {
+	public String profilec() {
 		return "Profile";
 	}
-
 }
