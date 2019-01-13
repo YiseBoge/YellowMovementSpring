47f05c6a29a7ad52d566f54aea5dee32779bac17
Nahu Haile <haile.nahu18@gmail.com>
2019-01-13 13:58:29 -0800

    front end jquery validation

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -23,3 +23,39 @@ $(function () {
 
     })
 })
+$(function(){
+	
+	$.validator.addMethod('strongPassword',function(value, element){
+		return this.optional(element)
+		|| value.length >= 6
+		&& /\d/.test(value)
+		&& /[a-z]/i.test(value);
+	},'Your password must be at least 6 characters long and contain at least one number.')
+	
+	$("createAccountForm").validate({
+		rules:{
+			signUpEmail:{
+				required:true,
+				email:true
+			
+				
+			},
+			signUpPassword:{
+				required:true,
+				strongPassword:true
+			},
+			signUpConfirmPassword:{
+				required:true,
+				equalTo: "#signUpPassword"
+			}
+	
+		},
+		messages:{
+			signUpEmail:{
+				required:'Please enter an email address.',
+				email: 'Please enter a <em>valid</em> email'
+				
+			}
+		}
+	})
+})
