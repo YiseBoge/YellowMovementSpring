daad9efd6dfabe28823d1b54cf2c289460b36bd4
mamushe <tewodroslemma64@gmail.com>
2019-02-03 19:59:50 +0300

    test class has been added to the userserviceimpl service class

--- src/main/java/com/yellowmovement/site/security/Role.java
+++ src/main/java/com/yellowmovement/site/security/Role.java
@@ -8,9 +8,13 @@ import javax.persistence.*;
 @Entity
 @Table(name = "roles")
 public class Role {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private String role;
+    public Role(String roleo) {
+    	this.role = roleo;
+    }
 }
